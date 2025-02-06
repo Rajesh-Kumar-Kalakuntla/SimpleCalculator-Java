e0bbb8d8c5ab6cec8a316f3a92f98e03bf0933a1
Kalakuntla Rajesh Kumar <kalakuntlarajeshkumar@gmail.com>
2025-02-06 15:16:56 +0530

    Performing Operations With The Result

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,47 +1,64 @@
 package calculator;
     import java.util.*;
     public class Calculator implements Calculator_Interface{
-    
-        @Override
-        public void calculation() {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("+ --> Addition");
-                System.out.println("- --> Subtraction");
-                System.out.println("* --> Multiplication");
-                System.out.println("/ --> Division");
-                System.out.println("Enter an Operator To Perform Operation: ");
-                String operator = sc.next();
-                System.out.println("Enter The First Number: ");
-                double First_Number = sc.nextDouble();
-                System.out.println("Enter The Second Number: ");
-                double Second_Number = sc.nextDouble();
-                double result;
-                switch (operator) {
-                    case "+":
-                         result = First_Number + Second_Number;
-                        System.out.println("Addition: " +result);
-                        break;
-                    case "-" :
-                         result = First_Number - Second_Number;
-                        System.out.println("Subtraction: " +result);
-                        break;
-                    case "*" :
-                         result = First_Number * Second_Number;
-                        System.out.println("Multiplication: " +result);
-                        break;
-                    case "/" :
-                         result = First_Number / Second_Number;
-                        System.out.println("Division: " +result);
-                        break;
-                    default:
-                        System.out.println("Invalid Operation");
-                        break;
+                                Scanner sc = new Scanner(System.in);
+                        @Override
+                        public void calculation(double First_Number , double Second_Number) {
+                                System.out.println("+ --> Addition");
+                                System.out.println("- --> Subtraction");
+                                System.out.println("* --> Multiplication");
+                                System.out.println("/ --> Division");
+                                System.out.println("Enter an Operator To Perform Operation: ");
+                                String operator = sc.next();
+                                if(First_Number == 0){
+                                System.out.println("Enter The First Number: ");
+                                First_Number = sc.nextDouble();
+                                }
+                                System.out.println("Enter The Second Number: ");
+                                Second_Number = sc.nextDouble();
+                                double result;
+                                switch (operator) {
+                                    case "+":
+                                         result = First_Number + Second_Number;
+                                        System.out.println("Addition: " +result);
+                                        Continue_Calculation(result);
+                                    case "-" :
+                                         result = First_Number - Second_Number;
+                                        System.out.println("Subtraction: " +result);
+                                        Continue_Calculation(result);
+                                    case "*" :
+                                         result = First_Number * Second_Number;
+                                        System.out.println("Multiplication: " +result);
+                                        Continue_Calculation(result);
+                                    case "/" :
+                                         result = First_Number / Second_Number;
+                                        System.out.println("Division: " +result);
+                                        Continue_Calculation(result);
+                                    default:
+                                        System.out.println("Invalid Opration");
+                                        Continue_Calculation(0);
+                                        break;
+                                }
+                                
+                            }
+                            @Override
+                            public void Continue_Calculation(double result) {
+                                System.out.println("DO You Want To Continue Performing Opeartions(Yes or No): ");
+                                String ans = sc.next();
+                                if(ans.equals("Yes")){
+                                    calculation(result,0);
+                                }
+                                    else{
+                                        System.out.println("The Previous Data Is Cleared");
+                                        System.out.println("Welcome!!!");
+                                        calculation(0, 0);
+                                    }
                 }
-            }
+            
             public static void main(String[] args) {
                 Calculator_Interface operation =new Calculator();
                 System.out.println("WELCOME!!!");
-                operation.calculation();
-    
+                double result = 0;
+                operation.calculation(result,0);
             }
 }
--- src/calculator/Calculator_Interface.java
+++ src/calculator/Calculator_Interface.java
@@ -1,5 +1,6 @@
 package calculator;
 
 public interface Calculator_Interface {
-public void calculation();
+public void calculation(double First_Number,double Second_Number);
+public void Continue_Calculation(double result);
 }
